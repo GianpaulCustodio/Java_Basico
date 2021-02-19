d1e2411e6f018c2bb05dabbe45168b62f0f6d64e
GianpaulCustodio <gianpaulcustodio1198@gmail.com>
2021-02-19 18:20:24 -0500

    Clase 8

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author giant
+ */
+public class JavaApplication1 {
+    
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        //Guardar Números:
+        //float numero;
+        //System.out.println("Digite un número: ");
+            //numero=entrada.nextInt(); //Para los datos Int
+        //numero = entrada.nextFloat(); //Guarda Float
+        //System.out.println("El número es: "+numero);
+        
+        //Guardar cadenas:
+        /*String cadena;
+        System.out.println("Ingrese cadena:");
+        cadena = entrada.nextLine();
+        System.out.println("Lo que escribió fue: "+cadena);*/
+        
+        //Guardar una letra
+        /*char letra;
+        System.out.println("Ingrese una letra: ");
+        letra = entrada.next().charAt(0);
+        System.out.println("Letra: "+letra);*/
+    }
+    
+}
